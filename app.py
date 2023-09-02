0094dee9320f80bf869b9b8f7c03815f12cf5ef9
omar algamel <123903790+algamelomer@users.noreply.github.com>
2023-09-02 03:58:48 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 import os
 import openpyxl
 
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -44,4 +45,5 @@ def grades():
     return render_template('index.html', message='Sitting number or college not found')
 
 if __name__ == '__main__':
-    app.run()
+        app.run(host='0.0.0.0', port=5000)
+
